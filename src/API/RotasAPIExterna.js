138eb6ef86d3ca84c524f5cded2d2966f7453768
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-08-31 14:25:05 -0300

    Concluído plataforma

--- src/API/RotasAPIExterna.js
+++ src/API/RotasAPIExterna.js
@@ -1,3 +1,6 @@
 export const endpoint = "https://ongames-7fkb.onrender.com";
 export const getPlataformas = "/plataformas";
+export const addPlataforma = "/plataformas/add";
+export const deletePlataforma = "/plataformas/delete/";
+export const updatePlataforma = "/plataformas/update/";
 
--- src/Pages/Plataforma/Plataformas.js
+++ src/Pages/Plataforma/Plataformas.js
@@ -1,49 +1,133 @@
-import { useEffect, useState } from "react";
-import { endpoint, getPlataformas } from "../../API/RotasAPIExterna.js";
+import React, { useEffect, useState } from "react";
+import { endpoint, getPlataformas, addPlataforma, deletePlataforma, updatePlataforma } from "../../API/RotasAPIExterna.js";
 import axios from "axios";
 import '../../global.css';
 import Menu from "../../Components/Menu.js";
 import { platAdd } from "./style.js";
 
 function Plataformas() {
-    const url = endpoint + getPlataformas;
     
-    const [plats, setPlats] = useState([]);
+    
+    const [addedPlatform, setAddedPlatform] = useState([]);
+    const [showAddForm, setShowAddForm] = useState(false); // To control the visibility of the add form
+    const [newTitle, setNewTitle] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [editingPlatform, setEditingPlatform] = useState(null);
+
 
     useEffect(() => {
-        axios.get(url)
+        const getAllUrl = endpoint + getPlataformas;
+        axios.get(getAllUrl)
             .then((response) => {
-                setPlats(response.data);
+                setAddedPlatform(response.data);
             })
             .catch((error) => {
                 console.log(error);
             });
-    }, []);
+    }, [plataformListUpdate]);
+
+    const handleAddClick = () => {
+        setShowAddForm(true);
+        setNewTitle("");
+        setErrorMessage("");
+        setSuccessMessage("");
+    };
+
+    function plataformListUpdate (){
+        return true;
+    };
+    
+    const handleDelete = (id, titulo) => {
+        const confirmation = window.confirm(`Tem certeza que deseja excluir a plataforma '${titulo}'?`);
+        
+        if (confirmation) {
+            const deleteUrl = `${endpoint}${deletePlataforma}${id}`;
+            axios.delete(deleteUrl)
+                .then(() => {
+                    plataformListUpdate();                   
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+    };    
+
+    const handleSave = () => {
+        const url = endpoint + (editingPlatform ? `${updatePlataforma + editingPlatform._id}` : addPlataforma);
+        const method = editingPlatform ? "PUT" : "POST";
+        const body = { titulo: newTitle };    
+        axios({
+            method: method,
+            url: url,
+            data: body
+        })
+        .then((response) => {
+            method === "POST" ? setSuccessMessage("Cadastro realizado com sucesso.") : setSuccessMessage("Cadastro atualizado com sucesso.");            
+            setNewTitle("");
+            setShowAddForm(false);
+            setEditingPlatform(null);
+            plataformListUpdate();
+        })
+        .catch((error) => {
+            console.log(error.response.data.mensagem);
+            if (error.response.status === 400) {
+                setErrorMessage(error.response.data.mensagem);
+            } else {
+                method === "POST" ? setErrorMessage("Erro ao cadastrar plataforma.") : setErrorMessage("Erro ao atualizar plataforma.");
+            }
+        });
+    };
+
+    const handleCancel = () => {
+        setShowAddForm(false);
+    };
+
+    const handleEditClick = (platform) => {
+        setEditingPlatform(platform);
+        setShowAddForm(true);
+        setNewTitle(platform.titulo);
+    };    
 
     return (
         <>
             <h3>Gerenciamento de plataformas</h3>
             <Menu />
-            <div data-bs-theme="dark" style={platAdd}>               
+            <div data-bs-theme="dark" style={platAdd}>
                 <h5>Plataformas cadastradas:</h5>
-                <button type="button" class="btn btn-dark">+</button>
+                <button type="button" className="btn btn-dark" onClick={handleAddClick}>+</button>
             </div>
+            {showAddForm && (
+                <div>
+                    <h5>Cadastrar nova plataforma:</h5>
+                    <input
+                        type="text"
+                        placeholder="Título"
+                        value={newTitle}
+                        onChange={(e) => setNewTitle(e.target.value)}
+                    />
+                    <button type="button" className="btn btn-primary" onClick={handleSave}>Salvar</button>
+                    <button type="button" className="btn btn-secondary" onClick={handleCancel}>Cancelar</button>
+                    {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+                    {successMessage && <div style={{ color: "green" }}>{successMessage}</div>}
+                </div>
+            )}
             <table className="table table-striped table-dark">
                 <thead>
                     <tr>
                         <th scope="col">id</th>
                         <th scope="col">Título</th>
-                        <th scope="col">Ação</th>
+                        <th scope="col" class='actions'>Editar / Excluir</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {plats.map(plat => (
+                    {addedPlatform.map(plat => (
                         <tr key={plat._id} scope="row">
                             <td>{plat._id}</td>
                             <td>{plat.titulo}</td>
-                            <td>
-                                <span className="material-symbols-outlined">edit</span>
-                                <span className="material-symbols-outlined">delete</span>
+                            <td class='actions'>
+                                <span className="material-symbols-outlined" onClick={() => handleEditClick(plat)}>edit</span>
+                                <span className="material-symbols-outlined" onClick={() => handleDelete(plat._id)}>delete</span>
                             </td>
                         </tr>
                     ))}
@@ -53,5 +137,4 @@ function Plataformas() {
         </>
     )
 }
-
 export default Plataformas;

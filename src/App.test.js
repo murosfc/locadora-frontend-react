fe59fb033c863574573f5d94c0ffcd7dc48ccc9c
Felipe Muros <84643909+murosfc@users.noreply.github.com>
2023-09-12 19:24:29 -0300

    tentativa arrumar teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import Error from './Pages/Error/Error.js';
-import Home from './Pages/Home/index.js';
-import Plataformas from './Pages/Plataforma/Plataformas.js';
-import Jogos from './Pages/Jogo/Jogos.js';
-import Login from './Pages/Login/Login.js';
-import Menu from './Components/Menu.js';
+const { render, screen } = require('@testing-library/react');
+const Error = require('./Pages/Error/Error.js');
+const Home = require('./Pages/Home/index.js');
+const Plataformas = require('./Pages/Plataformas/Plataformas.js');
+const Jogos = require('./Pages/Jogos/Jogos.js');
+const Login = require('./Pages/Login/Login.js');
+const Menu = require('./Components/Menu.js');
+const CadastroJogo = require('./Pages/CadasroJogos/CadastroJogos.js');
+const Contas = require('./Pages/Conta/Contas.js');
 
 
 test('Error Page', () => {
@@ -45,6 +47,18 @@ test('Plataformas Page', () => {
 
 test('Jogos Page', () => {
   render(<Jogos />);
-  const linkElement = screen.getByText(/Gerenciamento de jogos/i);
+  const linkElement = screen.getByText(/Jogos disponíveis para aluguel/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Cadastro Jogo Page', () => {
+  render(<CadastroJogo />);
+  const linkElement = screen.getByText(/Jogos cadastrados/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Contas Page', () => {
+  render(<Contas/>);
+  const linkElement = screen.getByText(/Gerenciamento de contas/i);
   expect(linkElement).toBeInTheDocument();
 });
